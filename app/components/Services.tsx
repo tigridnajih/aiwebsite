b49d44295cd6bc865592318df76b9e66738ad7f7
Tigrid <najih@tigrid.com>
2026-02-28 16:16:11 +0530

    PORTFOLIO BORDER

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -17,7 +17,7 @@ export default function Services() {
                         {/* Left: Bubble Column */}
                         <div className="w-full md:flex-[1.8] flex flex-col gap-3 md:gap-4">
                             {/* Emma - Top Bubble (Replying) */}
-                            <div className="relative bg-gradient-to-br from-[#0B1F5C] to-[#040816] border border-blue-500/30 rounded-2xl p-4 shadow-[0_10px_30px_rgba(0,0,0,0.5)] transform hover:-translate-y-1 transition-transform duration-500 group/emma">
+                            <div className="relative bg-gradient-to-br from-[#3B5BFF] to-[#000000] border border-[#3B5BFF]/30 rounded-2xl p-4 shadow-[0_10px_30px_rgba(0,0,0,0.5)] transform hover:-translate-y-1 transition-transform duration-500 group/emma">
                                 <div className="flex items-start gap-3">
                                     <div className="relative shrink-0">
                                         <div className="w-12 h-12 rounded-xl bg-zinc-800 overflow-hidden border border-white/[0.06]">
@@ -27,7 +27,7 @@ export default function Services() {
                                                 className="w-full h-full object-cover"
                                             />
                                         </div>
-                                        <div className="absolute -bottom-1 -right-1 w-5 h-5 bg-[#25D366] rounded-full border-2 border-[#0B1F5C] flex items-center justify-center shadow-lg">
+                                        <div className="absolute -bottom-1 -right-1 w-5 h-5 bg-[#25D366] rounded-full border-2 border-[#3B5BFF] flex items-center justify-center shadow-lg">
                                             <svg viewBox="0 0 24 24" className="w-2.5 h-2.5 fill-white" xmlns="http://www.w3.org/2000/svg"><path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z" /></svg>
                                         </div>
                                     </div>
